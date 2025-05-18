51b73034139e68c08a55a5e60f6ae22475db1aae
Cédric Guetté <cedric.guette@gmail.com>
2025-05-18 15:48:06 +0200

    Initial commit

---
+++ src/test/java/com/jeuxolympiques/billetterie/BilletterieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeuxolympiques.billetterie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilletterieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
